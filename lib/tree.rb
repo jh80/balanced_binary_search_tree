8a6b8161489fdef19435059d3765fd8fe96f2538
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-02-26 10:55:15 -0800

    Build BTS from array

--- lib/tree.rb
+++ lib/tree.rb
@@ -4,6 +4,23 @@ require "./lib/node.rb"
 
 class Tree 
   def initialize(unfiltered_array)
-    @root = build_tree(unfiltered_array.uniq.sort)
+    @array = unfiltered_array.uniq.sort
+    @root = build_tree(@array, 0, @array.length - 1)
+  end
+
+  def build_tree(array, start, stop)
+    return nil if start > stop
+      
+    middle = start + ((stop - start)/2).floor
+    root = Node.new(array[middle])
+    root.left = build_tree(array, start, middle - 1)
+    root.right = build_tree(array, middle + 1, stop)
+    root
+  end
+
+  def pretty_print(node = @root, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
   end
 end
