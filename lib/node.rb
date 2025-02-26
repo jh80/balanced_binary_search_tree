b48e80d9eb78fd9d5632d4b9d6c149fd454f8b32
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-02-26 11:50:11 -0800

    Pretty print Nodes
    This may only work on nodes that are the root node
    of an initialized tree. Or maybe just middle nodes.

--- lib/node.rb
+++ lib/node.rb
@@ -10,4 +10,10 @@ class Node
     @left = nil
     @right = nil
   end
+
+  def pretty_print(node = self, prefix = '', is_left = true)
+    pretty_print(node.right, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right
+    puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
+    pretty_print(node.left, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left
+  end
 end
