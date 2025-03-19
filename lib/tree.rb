5e82947206b8bef6c516d1bc4ebb71fd07fed2b9
Jennifer H <74622778+jh80@users.noreply.github.com>
2025-03-19 15:09:06 -0700

    Conserve stack memory with iterative level_order

--- lib/tree.rb
+++ lib/tree.rb
@@ -99,6 +99,28 @@ class Tree
     @root.count_edges_to(node)
   end
 
+  def level_order_i
+    queue = [@root]
+    while !queue.empty?
+      curr = queue.shift
+      curr.add_children_to(queue)
+      yield(curr)
+    end
+
+    # queue = [@root]
+    # spare_array = block_given? ? [] : [@root]
+    # while !queue.empty?
+    #   node = queue.shift
+    #   node.add_children_to(queue)
+    #   if block_given?
+    #     yield(node, array = spare_array)
+    #   else
+    #     node.add_children_to(spare_array)
+    #   end
+    # end
+    # return spare_array unless block_given?
+  end
+
   def build_tree(array, start, stop)
     return nil if start > stop
       
